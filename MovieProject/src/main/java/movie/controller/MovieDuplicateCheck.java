67aa2b3083936d8064a3f285137849b4cb9f809c
김홍비 <@DESKTOP-TMJ4ATA>
2024-12-17 14:07:03 +0900

    영화등록페이지 MovieRegister 관련 작업

---
+++ MovieProject/src/main/java/movie/controller/MovieDuplicateCheck.java
@@ -0,0 +1,15 @@
+package movie.controller;
+
+import common.controller.AbstractController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MovieDuplicateCheck extends AbstractController {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MovieProject/src/main/java/movie/model/MovieDAO.java
@@ -0,0 +1,12 @@
+package movie.model;
+
+import java.sql.SQLException;
+
+import movie.domain.MovieVO;
+
+public interface MovieDAO {
+	
+	// 영화를 등록해주는 메소드(tbl_movie 테이블에 insert)
+	int registerMovie(MovieVO movie) throws SQLException;
+
+}
