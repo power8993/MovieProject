a155403866bc88df29d9c890de337ef3fdde1672
user <user@121.140.155.199>
2025-01-07 01:05:13 +0900

    2025_01_07 이상우 집

--- MovieProject/src/main/java/common/model/indexDAO_imple.java
+++ MovieProject/src/main/java/common/model/indexDAO_imple.java
@@ -179,12 +179,13 @@ public class indexDAO_imple implements indexDAO{
 		try {
 			conn = ds.getConnection();
 
-			String sql = " SELECT SEQ_SHOWTIME_NO, SEQ_MOVIE_NO, MOVIE_TITLE, POSTER_FILE "
-					+ " FROM (\n"
+			String sql = " SELECT SEQ_SHOWTIME_NO, SEQ_MOVIE_NO, MOVIE_TITLE, POSTER_FILE,MOVIE_GRADE "
+					+ " FROM ( "
 					+ "    SELECT DISTINCT S.SEQ_SHOWTIME_NO, "
 					+ "           M.SEQ_MOVIE_NO, "
 					+ "           M.MOVIE_TITLE, "
 					+ "           M.POSTER_FILE, "
+					+ "			  M.MOVIE_GRADE, "
 					+ "           COUNT(P.IMP_UID) AS BOOKING_COUNT, "
 					+ "           ROW_NUMBER() OVER (ORDER BY COUNT(P.IMP_UID) DESC) AS RN "
 					+ "    FROM TBL_MOVIE M "
@@ -192,8 +193,8 @@ public class indexDAO_imple implements indexDAO{
 					+ "      ON M.SEQ_MOVIE_NO = S.FK_SEQ_MOVIE_NO "
 					+ "    LEFT JOIN TBL_PAYMENT P "
 					+ "      ON S.SEQ_SHOWTIME_NO = P.FK_SEQ_SHOWTIME_NO "
-					+ "    GROUP BY S.SEQ_SHOWTIME_NO, M.SEQ_MOVIE_NO, M.MOVIE_TITLE, M.POSTER_FILE "
-					+ " )  "
+					+ "    GROUP BY S.SEQ_SHOWTIME_NO, M.SEQ_MOVIE_NO, M.MOVIE_TITLE, M.POSTER_FILE,M.MOVIE_GRADE "
+					+ " ) "
 					+ " WHERE RN > 5 AND RN <= 10 ";
 
 			pstmt = conn.prepareStatement(sql);
@@ -205,6 +206,7 @@ public class indexDAO_imple implements indexDAO{
 				MovieVO2.setSeq_movie_no(rs.getInt("seq_movie_no"));
 				MovieVO2.setMovie_title(rs.getString("MOVIE_TITLE"));
 				MovieVO2.setPoster_file(rs.getString("POSTER_FILE"));
+				MovieVO2.setMovie_grade(rs.getString("MOVIE_GRADE"));
 				
 				ShowtimeVO ShowtimeVO2 = new ShowtimeVO();
 				ShowtimeVO2.setSeq_showtime_no(rs.getInt("SEQ_SHOWTIME_NO")); //ShowtimeVO에 '상영중인영화번호'를 삽입
@@ -232,7 +234,7 @@ public class indexDAO_imple implements indexDAO{
 		try {
 			conn = ds.getConnection();
 
-			String sql = " select  seq_movie_no, MOVIE_TITLE, POSTER_FILE from TBL_MOVIE "
+			String sql = " select  seq_movie_no, MOVIE_TITLE, POSTER_FILE, MOVIE_GRADE,round(start_date-sysdate) as remaining_day from TBL_MOVIE "
 					+ " where start_date>sysdate "
 					+ " and ROWNUM<=5 "
 					+ " order by start_date ";
@@ -246,7 +248,8 @@ public class indexDAO_imple implements indexDAO{
 				MovieVO.setSeq_movie_no(rs.getInt("seq_movie_no"));
 				MovieVO.setMovie_title(rs.getString("MOVIE_TITLE"));
 				MovieVO.setPoster_file(rs.getString("POSTER_FILE"));
-				
+				MovieVO.setRemaining_day(rs.getInt("remaining_day"));
+				MovieVO.setMovie_grade(rs.getString("MOVIE_GRADE"));
 				laterMoviesList.add(MovieVO);
 				
 			} // end of while------------------
@@ -268,9 +271,9 @@ public class indexDAO_imple implements indexDAO{
 		try {
 			conn = ds.getConnection();
 
-			String sql = " SELECT seq_movie_no, MOVIE_TITLE, POSTER_FILE "
+			String sql = " SELECT seq_movie_no, MOVIE_TITLE, POSTER_FILE,MOVIE_GRADE,round(start_date-sysdate) as remaining_day "
 					+ " FROM ( "
-					+ "    SELECT seq_movie_no, MOVIE_TITLE, POSTER_FILE, ROWNUM AS RN "
+					+ "    SELECT seq_movie_no, MOVIE_TITLE, POSTER_FILE,start_date,MOVIE_GRADE, ROWNUM AS RN "
 					+ "    FROM TBL_MOVIE "
 					+ "    WHERE start_date > SYSDATE "
 					+ "    ORDER BY start_date "
@@ -286,6 +289,8 @@ public class indexDAO_imple implements indexDAO{
 				MovieVO.setSeq_movie_no(rs.getInt("seq_movie_no"));
 				MovieVO.setMovie_title(rs.getString("MOVIE_TITLE"));
 				MovieVO.setPoster_file(rs.getString("POSTER_FILE"));
+				MovieVO.setRemaining_day(rs.getInt("remaining_day"));
+				MovieVO.setMovie_grade(rs.getString("MOVIE_GRADE"));
 				
 				laterMoviesList2.add(MovieVO);
 				
